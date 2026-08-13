911e933471348e9112cde4565c469265345ba999
Dayumun <diego.ayumun@educa.jcyl.es>
2026-08-14 00:12:10 +0200

    Primer commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '8868196951:AAF_JyAhSvvTZ_EdE2sUiCDpQSZkIjnOLxs'
+
+BOT_USERNAME: Final = '@weatherPiPi_bot'
